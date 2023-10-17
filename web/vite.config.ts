b79248aa3ecf3b4eb052c92c0c7e70798add5654
mariz <marrise@oostvoort.com>
2023-10-17 16:40:18 +0800

    refactor: get manifest.json from forkserver

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
         target: 'http://localhost:3000/',
         changeOrigin: true,
       },
+      '/world': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+      },
     },
   },
   resolve: {
